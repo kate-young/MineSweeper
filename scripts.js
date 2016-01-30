342136769714ae492ac3b89845772ba0161e822e
kate-young <S1391628@student.mcckc.edu>
2016-01-30 12:27:06 -0600

    Update board so that mines are randomly placed

--- scripts.js
+++ scripts.js
@@ -7,19 +7,27 @@ var Board = function(rows, columns, mines) {
 
     this.fields = new Map();
 
+    this.generate_mine_field = function() {
+        var rands = new Array();
+        for(var i = 0; i < this.mines; i++) {
+            rands.push(Math.floor(Math.random() * this.mines));
+        }
+        return rands;
+    }
+
     this.initialize = function() {
         var counter = 0;
-        var mines = mine_field();
         var $board = $("#main");
+        var mine_field = this.generate_mine_field();
 
         for(var r = 0; r < this.rows; r++) {
             var $row = $("<div class='row'></div>");
             for(var c = 0; c < this.columns; c++) {
-                var field = new Field(r, c, mines.indexOf(counter) > -1);
                 var $button = $("<button class='btn btn-default field'> </button>");
-                $button.attr("id", counter);
+                var field = new Field(r, c, mine_field.indexOf(counter) > -1);
+                $button.attr("id", r + " " + c);
                 $button.appendTo($row);
-                this.fields.set(field, $button);
+                this.fields.set(r + " " + c, field);
                 counter++;
             }
             $row.appendTo($board);
@@ -27,12 +35,8 @@ var Board = function(rows, columns, mines) {
         return $board;
     }
 
-    var mine_field = function() {
-        var rands = new Array();
-        for(var i = 0; i < this.mines; i++) {
-            rands.push(Math.floor(Math.random() * this.mines));
-        }
-        return rands;
+    this.get_field = function(coords) {
+        return this.fields.get(coords);
     }
 }
 
@@ -46,9 +50,22 @@ var Field = function(row, column, mine) {
    this.revealed = false;
 
    this.marked = false;
+
+   this.reveal = function() {
+       this.revealed = true;
+   }
+
+   this.mark = function() {
+       this.marked = true;
+   }
 }
 
 $(document).ready(function() {
-    board = new Board(10, 10, 2);
+    board = new Board(10, 10, 50);
     board.initialize().appendTo("body");
+    $(".btn").on("click", function() {
+        var field = board.get_field($(this).attr("id"));
+        field.reveal();
+        console.log(field);
+    });
 });
