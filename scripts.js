c759f16808406dc65f9c99bcaf9a58c19c56096d
kate-young <S1391628@student.mcckc.edu>
2016-01-30 10:17:47 -0600

    Show board on screen

--- scripts.js
+++ scripts.js
@@ -4,11 +4,28 @@ var Board = function(rows, columns, mines) {
     this.mines = mines;
     this.fields = new Array();
     this.initialize = function() {
+        var counter = 0;
+        var mines = mine_field();
+        var mine;
+        var board = "<div class='board'>";
         for(var r = 0; r < this.rows; r++) {
             for(var c = 0; c < this.columns; c++) {
-                this.fields.push(new Field(r, c, false));              
+                mine = mines.indexOf(counter) > -1; 
+                this.fields.push(new Field(r, c, mine));              
+                board += "<button class='btn btn-default field'> </button>";
+                counter++;
             }
+            board += "</br>";
         }
+        board += "</div>";
+        return board;
+    }
+    var mine_field = function() {
+        var rands = new Array();
+        for(var i = 0; i < this.mines; i++) {
+            rands.push(Math.floor(Math.random() * this.mines));
+        }
+        return rands;
     }
 }
 
@@ -16,4 +33,18 @@ var Field = function(row, column, mine) {
    this.row = row;
    this.column = column;
    this.mine = mine;
+   this.revealed = false;
+   this.marked = false;
+   this.icon = function() {
+       if(this.mine) {
+           return "<span class='glyphicon glyphicon-alert'><span>";
+       }
+    }
 }
+
+$(document).ready(function() {
+    board = new Board(10, 10, 2);
+    $("#main").html(board.initialize());
+    $(".field").on("click", function() {
+    });
+});
