3ef419fa27c21f8176c066b1f4f71cc23fd2c390
kate-young <S1391628@student.mcckc.edu>
2016-01-30 09:21:37 -0600

    Build out board and field classes

--- scripts.js
+++ scripts.js
@@ -1,10 +1,19 @@
-var board = {
-
+var Board = function(rows, columns, mines) {
+    this.rows = rows;
+    this.columns = columns;
+    this.mines = mines;
+    this.fields = new Array();
+    this.initialize = function() {
+        for(var r = 0; r < this.rows; r++) {
+            for(var c = 0; c < this.columns; c++) {
+                this.fields.push(new Field(r, c, false));              
+            }
+        }
+    }
 }
 
-var mine = {
-
-}
-
-var safe = {
+var Field = function(row, column, mine) {
+   this.row = row;
+   this.column = column;
+   this.mine = mine;
 }
