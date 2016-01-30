1e915767cd24f8f58a5b95de3aad8377844d0dfe
kate-young <S1391628@student.mcckc.edu>
2016-01-30 11:23:12 -0600

    Create map of board elements

--- scripts.js
+++ scripts.js
@@ -1,25 +1,32 @@
 var Board = function(rows, columns, mines) {
     this.rows = rows;
+
     this.columns = columns;
+
     this.mines = mines;
-    this.fields = new Array();
+
+    this.fields = new Map();
+
     this.initialize = function() {
         var counter = 0;
         var mines = mine_field();
-        var mine;
-        var board = "<div class='board'>";
+        var $board = $("#main");
+
         for(var r = 0; r < this.rows; r++) {
+            var $row = $("<div class='row'></div>");
             for(var c = 0; c < this.columns; c++) {
-                mine = mines.indexOf(counter) > -1; 
-                this.fields.push(new Field(r, c, mine));              
-                board += "<button class='btn btn-default field'> </button>";
+                var field = new Field(r, c, mines.indexOf(counter) > -1);
+                var $button = $("<button class='btn btn-default field'> </button>");
+                $button.attr("id", counter);
+                $button.appendTo($row);
+                this.fields.set(field, $button);
                 counter++;
             }
-            board += "</br>";
+            $row.appendTo($board);
         }
-        board += "</div>";
-        return board;
+        return $board;
     }
+
     var mine_field = function() {
         var rands = new Array();
         for(var i = 0; i < this.mines; i++) {
@@ -31,20 +38,17 @@ var Board = function(rows, columns, mines) {
 
 var Field = function(row, column, mine) {
    this.row = row;
+
    this.column = column;
+
    this.mine = mine;
+
    this.revealed = false;
+
    this.marked = false;
-   this.icon = function() {
-       if(this.mine) {
-           return "<span class='glyphicon glyphicon-alert'><span>";
-       }
-    }
 }
 
 $(document).ready(function() {
     board = new Board(10, 10, 2);
-    $("#main").html(board.initialize());
-    $(".field").on("click", function() {
-    });
+    board.initialize().appendTo("body");
 });
